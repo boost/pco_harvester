4c21508ddc4078de2b85b59756fd3786b0ba8bcf
Richard Matthews <richard@boost.co.nz>
2023-04-18 16:36:45 +1200

    Add basic validation tests

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem "cssbundling-rails"
 
 gem 'foreman'
 
+gem 'pry-byebug'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- app/controllers/content_partners_controller.rb
+++ app/controllers/content_partners_controller.rb
@@ -2,6 +2,21 @@
 
 class ContentPartnersController < ApplicationController
   def index
+    @content_partners = ContentPartner.all
+    @content_partner  = ContentPartner.new 
+  end
 
+  def create
+    @content_partner = ContentPartner.create(content_partner_params)
+
+    @content_partner.save!
+
+    redirect_to content_partners_path
+  end
+
+  private
+
+  def content_partner_params
+    params.require(:content_partner).permit(:name)
   end
 end
