9d1358bbacdfd64e4fed41f6efe2ee6024b901f3
Richard Matthews <richard@boost.co.nz>
2023-04-18 15:02:14 +1200

    Add bootstrap JS dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 
+const path = require('path')
+
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 })
   ],
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
 })
