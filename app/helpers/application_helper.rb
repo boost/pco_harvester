aa62a81df785bd2bb60ce1c6fbd75db96ae1b33b
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-04-19 16:55:33 +0200

    refactor: improved the breadcrumb generation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_page(controller, actions)
     controller_name == controller.to_s && action_name.in?(actions)
   end
+
+  def breadcrumb_item(text:, path: nil, display: true, active: false)
+    return unless display
+
+    tag.li(class: { 'breadcrumb-item': true, active: }) do
+      path && !active ? link_to(text, path) : text
+    end
+  end
 end
