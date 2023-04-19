5dea3416fbc4a930ecc618d5dba1f006b4862177
Richard Matthews <richard@boost.co.nz>
2023-04-19 13:33:52 +1200

    feat(create extraction): Edit Extraction
    Add the ability to edit an extraction.

--- app/controllers/extraction_definitions_controller.rb
+++ app/controllers/extraction_definitions_controller.rb
@@ -2,7 +2,7 @@
 
 class ExtractionDefinitionsController < ApplicationController
   before_action :find_content_partner
-  before_action :find_extraction_definition, only: %i[show]
+  before_action :find_extraction_definition, only: %i[show edit]
 
   def show
   end
@@ -12,12 +12,27 @@ class ExtractionDefinitionsController < ApplicationController
   end
 
   def create
-    @extraction_definition = ExtractionDefinition.new(extraction_definitions_params)
+    @extraction_definition = ExtractionDefinition.new(extraction_definition_params)
     if @extraction_definition.save
       redirect_to content_partner_path(@content_partner), notice: 'Extraction Definition created successfully'
     else
       flash.alert = 'There was an issue creating your Extraction Definition'
       render :new
+    end
+  end
+
+  def edit
+
+  end
+  
+  def update
+    @extraction_definition = ExtractionDefinition.find(params[:id])
+
+    if @extraction_definition.update(extraction_definition_params)
+      redirect_to content_partner_extraction_definition_path(@content_partner, @extraction_definition), notice: 'Extraction Definition updated successfully'
+    else
+      flash.alert = 'There was an issue updating your Extraction Definition'
+      render 'edit'
     end
   end
 
@@ -31,7 +46,7 @@ class ExtractionDefinitionsController < ApplicationController
     @extraction_definition = ExtractionDefinition.find(params[:id])
   end
 
-  def extraction_definitions_params
+  def extraction_definition_params
     params.require(:extraction_definition).permit(:content_partner_id, :name)
   end
 end
