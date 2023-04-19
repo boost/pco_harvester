876d03ea34e0574aa8745b0bafd34d9dff3effc0
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-04-19 17:16:31 +0200

    refactor: used a before_action to find content partner

--- app/controllers/content_partners_controller.rb
+++ app/controllers/content_partners_controller.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 class ContentPartnersController < ApplicationController
+  before_action :find_content_partner, only: %i[show edit update]
+
   def index
     @content_partners = ContentPartner.order(:name).page(params[:page])
   end
 
-  def new
-    @content_partner  = ContentPartner.new
-  end
-
   def show
-    @content_partner = ContentPartner.find(params[:id])
     @extraction_definitions = @content_partner.extraction_definitions.order(created_at: :desc).page(params[:page])
   end
 
-  def edit
-    @content_partner = ContentPartner.find(params[:id])
+  def new
+    @content_partner = ContentPartner.new
   end
 
   def create
@@ -29,9 +26,9 @@ class ContentPartnersController < ApplicationController
     end
   end
 
-  def update
-    @content_partner = ContentPartner.find(params[:id])
+  def edit; end
 
+  def update
     if @content_partner.update(content_partner_params)
       redirect_to content_partners_path, notice: 'Content Partner updated successfully'
     else
@@ -44,5 +41,9 @@ class ContentPartnersController < ApplicationController
 
   def content_partner_params
     params.require(:content_partner).permit(:name)
+  end
+
+  def find_content_partner
+    @content_partner = ContentPartner.find(params[:id])
   end
 end
