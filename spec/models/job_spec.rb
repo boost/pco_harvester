168478b479c7847a6fae143409c9e2246034dc74
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-04-25 00:36:15 +0200

    test: added tests for the job model

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'status checks' do
+    subject { create(:job) }
+
+    Job::STATUSES.each do |status|
+      it "defines the check #{status}?" do
+        subject.status = status
+        expect(subject.send("#{status}?")).to be true
+        subject.status = 'test'
+        expect(subject.send("#{status}?")).to be false
+      end
+
+      it "defines a way to update the status with mark_as_#{status}" do
+        subject.status = status
+        expect(subject.send("mark_as_#{status}")).to be true
+        subject.reload
+        expect(subject.send("#{status}?")).to be true
+      end
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:extraction_definition).with_message('must exist') }
+    it { should validate_inclusion_of(:status).in_array(Job::STATUSES).with_message('is not included in the list') }
+  end
 end
