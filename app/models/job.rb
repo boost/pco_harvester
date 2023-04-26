0a7eea68e09d21f84f6951374865a8a0042b1c4e
Richard Matthews <richard@boost.co.nz>
2023-04-26 15:51:52 +1200

    test(perform_extraction): Job Model Specs
    Add further specs to the Job Model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,3 @@
-# This may not be the best name as it could cause confusion between this 'Job History Object' and the Sidekiq 'Job' concept
 
 class Job < ApplicationRecord
   EXTRACTIONS_FOLDER = "#{Rails.root}/extractions".freeze
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  describe 'status checks' do
-    subject { create(:job) }
+  subject { create(:job) }
 
+  describe 'status checks' do
     Job::STATUSES.each do |status|
       it "defines the check #{status}?" do
         subject.status = status
@@ -21,8 +21,54 @@ RSpec.describe Job, type: :model do
     end
   end
 
+  describe 'kind checks' do
+    Job::KINDS.each do |kind|
+      it "defines the check #{kind}?" do
+        subject.kind = kind
+        expect(subject.send("#{kind}?")).to be true
+        subject.kind = 'test'
+        expect(subject.send("#{kind}?")).to be false
+      end
+    end
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:extraction_definition).with_message('must exist') }
     it { should validate_inclusion_of(:status).in_array(Job::STATUSES).with_message('is not included in the list') }
+    it { should validate_inclusion_of(:kind).in_array(Job::KINDS).with_message('is not included in the list') }
+  end
+
+  describe '#associations' do
+    it 'belongs to an Extraction Definition' do
+      expect(subject.extraction_definition).to be_a(ExtractionDefinition)
+    end
+  end
+
+  describe '#extraction_folder' do
+    it 'returns the path for where the extractions are kept' do
+      expect(subject.extraction_folder).to eq("#{Rails.root.join('extractions')}/#{subject.created_at.strftime('%Y-%m-%d_%H-%M-%S')}_-_#{subject.id}")
+    end
+  end
+
+  describe '#create_folder' do
+    it 'creates a folder at the Extractions Path' do
+      expect(File.exist?(subject.extraction_folder)).to eq true
+    end
+  end
+
+  describe '#delete_folder' do
+    it 'deletes a folder at the extractions path' do
+      expect(File.exist?(subject.extraction_folder)).to eq true
+
+      subject.delete_folder
+
+      expect(File.exist?(subject.extraction_folder)).to eq false
+    end
+  end
+
+  describe '#documents' do
+    it 'returns an Extraction::Documents object' do
+      expect(subject.documents).to be_a(Extraction::Documents)
+    end
   end
 end
