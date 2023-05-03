aed9f8d25472c5ee9671e3b22d345222dff176e5
Richard Matthews <richard@boost.co.nz>
2023-05-03 16:45:35 +1200

    Initial implementation for applying the transformation attributes to the records

--- app/models/transformation.rb
+++ app/models/transformation.rb
@@ -13,4 +13,20 @@ class Transformation < ApplicationRecord
             .on(job.documents[1].body)
             .flatten
   end
+
+  def transformed_records
+    # TODO rename attribute class as it conflicts with 
+    # the attributes method from active record :(
+
+    # Very basic initial implementation
+
+    records.map do |record|
+      Attribute.where(transformation_id: id).each_with_object({}) do |attribute, hash|
+        block = ->(record) { eval(attribute.block) }
+
+        hash[attribute.name] = block.call(record)
+      end
+    end
+    
+  end
  end
--- spec/models/transformation_spec.rb
+++ spec/models/transformation_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Transformation, type: :model do
   let(:extraction_definition) { content_partner.extraction_definitions.first }
   let(:job)             { create(:job, extraction_definition:) } 
   let(:subject)         { create(:transformation, content_partner: content_partner, job: job) }
+
+  let!(:attribute_one)   { create(:attribute, name: 'title', block: "JsonPath.new('title').on(record).first", transformation: subject) }
+  let!(:attribute_two)   { create(:attribute, name: 'source', block: "JsonPath.new('source').on(record).first", transformation: subject) }
+  let!(:attribute_three) { create(:attribute, name: 'dc_identifier', block: "JsonPath.new('reference_number').on(record).first", transformation: subject) }
+  let!(:attribute_four) { create(:attribute, name: 'landing_url', block: '"http://www.ngataonga.org.nz/collections/catalogue/catalogue-item?record_id=#{record[\'record_id\']}"', transformation: subject) }
   
   before do
     # that's to test the display of results
@@ -33,6 +38,17 @@ RSpec.describe Transformation, type: :model do
   describe '#records' do
     it 'returns the records from the job documents' do
       expect(subject.records.first).to have_key 'record_id'
+    end
+  end
+
+  describe '#transformed_records' do
+    it 'returns the records having applied the attributes to them' do
+      subject.records.zip(subject.transformed_records).each do |record, transformed_record|
+        expect(transformed_record['title']).to eq record['title']
+        expect(transformed_record['source']).to eq record['source']
+        expect(transformed_record['dc_identifier']).to eq record['reference_number']
+        expect(transformed_record['landing_url']).to eq "http://www.ngataonga.org.nz/collections/catalogue/catalogue-item?record_id=#{record['record_id']}"
+      end
     end
   end
   
