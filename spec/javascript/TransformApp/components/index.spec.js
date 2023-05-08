b2815c2372800481d81d1308e378097416f1e8a9
Richard Matthews <richard@boost.co.nz>
2023-05-08 16:39:14 +1200

    Attempt to make jest work with redux

--- spec/javascript/TransformApp/components/index.spec.js
+++ spec/javascript/TransformApp/components/index.spec.js
@@ -1,7 +1,12 @@
-import renderer from 'react-test-renderer';
+import React from 'react'
+import { fireEvent, screen } from '@testing-library/react'
 import TransformationApp from 'js/apps/TransformationApp/index';
+import { renderWithProviders } from '../../test-utils';
 
 describe("<TransformationApp />", () => {
   it("runs", () => {
+    renderWithProviders(<TransformationApp />);
+
+    
   });
 });
