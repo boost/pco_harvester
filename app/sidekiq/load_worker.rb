75cc15dc2466b01c51f3fc3b6bb00f4b53b297f0
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-05-16 22:12:29 +0200

    fix(run_harvest): reloaded the TransformationLoad object to have more accurate numbers

--- app/sidekiq/load_worker.rb
+++ app/sidekiq/load_worker.rb
@@ -5,10 +5,16 @@ class LoadWorker < ApplicationWorker
 
   def child_perform(load_job, harvest_job_id, transformed_records)
     harvest_job = HarvestJob.find(harvest_job_id)
+    sent_records = 0
     transformed_records.each do |transformed_record|
       Load::Execution.new(transformed_record, harvest_job.destination).call
-      load_job.records_sent_to_api += 1
-      load_job.save!
+      sent_records += 1
     end
+    update_load_report(load_job, sent_records)
+  end
+
+  def update_load_report(load_job, sent_records)
+    load_job.reload
+    load_job.update(records_sent_to_api: load_job.records_sent_to_api + sent_records)
   end
 end
