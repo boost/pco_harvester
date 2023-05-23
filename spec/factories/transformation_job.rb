07bb533d45437546b1f7602057b7e6b4d623da09
Richard Matthews <richard@boost.co.nz>
2023-05-24 08:27:26 +1200

    Code review feedback

--- spec/factories/transformation_job.rb
+++ spec/factories/transformation_job.rb
@@ -5,11 +5,6 @@ FactoryBot.define do
     status { 'queued' }
     page { 1 }
 
-    # before(:create) do |transformation_job|
-    #   binding.pry
-    #   transformation_job.transformation_definition = create(:transformation_definition)
-    # end
-
     transformation_definition
     harvest_job
     extraction_job
