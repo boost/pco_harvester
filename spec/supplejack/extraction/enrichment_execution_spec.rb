4ff7446f4928aedd138fd8cd5cc23ea455b6ea74
Richard Matthews <richard@boost.co.nz>
2023-05-25 11:01:22 +1200

    Add spec for enrichment executions respecting a throttle

--- spec/supplejack/extraction/enrichment_execution_spec.rb
+++ spec/supplejack/extraction/enrichment_execution_spec.rb
@@ -39,5 +39,21 @@ RSpec.describe Extraction::EnrichmentExecution do
         expect(extracted_files.count).to eq 40
       end
     end
+
+    context 'when the enrichment extraction definition has a throttle' do
+      let(:extraction_definition) { create(:extraction_definition, :enrichment, destination:, throttle: 50) }
+      let(:job) { create(:extraction_job, extraction_definition: extraction_definition, kind: 'sample') }
+
+      it 'respects the throttle set in the extraction definition' do
+        start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+        described_class.new(job).call
+
+        end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+        total_time = end_time - start_time
+
+        expect(total_time.ceil).to eq 2
+      end
+    end
   end
 end
