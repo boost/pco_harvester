91cccebde6166cedb81b49e44ec435d6dea33ab0
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-06-05 23:11:05 +0200

    feat(user_account): show view of user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,7 @@ class UsersController < ApplicationController
     @users = User.order(username: :desc).page(params[:page])
   end
 
-  def new; end
-  def edit; end
-  def create; end
-  def update; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
