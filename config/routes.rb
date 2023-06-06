ffc7d08590dd49e50d99fb55fe840c34d91d58ec
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-06-07 00:56:12 +0200

    feat(user_account): ability to cancel my account

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_for :users, :skip => [:registrations]
   as :user do
     get 'users/edit' => 'devise/registrations#edit', as: :edit_user_registration
-    put 'users/:id' => 'devise/registrations#update', as: :registration
+    put 'users/:id' => 'devise/registrations#update', as: :user_registration
+    delete 'users' => 'devise/registrations#destroy', as: :registration
   end
 
   root 'home#index'
