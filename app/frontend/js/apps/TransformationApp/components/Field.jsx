3b3aa0e44a2021775f2cb1ab1fe9217d28fa6d34
Richard Matthews <richard@boost.co.nz>
2023-06-07 14:14:48 +1200

    feat(transformation_page): Delete Confirmation Modal
    Added a delete modal to ask for confirmation before deleting a field.
    :)

--- app/frontend/js/apps/TransformationApp/components/Field.jsx
+++ app/frontend/js/apps/TransformationApp/components/Field.jsx
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import classNames from "classnames";
 import { isEmpty } from 'lodash';
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 import { updateField, deleteField } from "~/js/features/FieldsSlice";
 import { selectFieldById } from "~/js/features/FieldsSlice";
 import {
@@ -42,6 +45,7 @@ const Field = ({ id }) => {
 
   const [nameValue, setNameValue] = useState(name);
   const [blockValue, setBlockValue] = useState(block);
+  const [show, setShow] = useState(false);
   
   const uiAppDetails = useSelector(selectUiAppDetails);
   const { readOnly } = uiAppDetails;
@@ -64,6 +68,7 @@ const Field = ({ id }) => {
         transformationDefinitionId: appDetails.transformationDefinition.id,
       })
     );
+    handleClose();
   };
 
   const handleCollapseExpandClick = () => {
@@ -140,91 +145,112 @@ const Field = ({ id }) => {
 
   const fieldClasses = classNames('col-12', 'collapse', { 'show': displayed });
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div id={`field-${id}`} className={fieldClasses} data-testid="field">
-      <div className="card">
-        <div className="card-body">
-          <div className="d-flex d-row justify-content-between align-items-center">
-            <div>
-              <h5 className="m-0 d-inline">{name}</h5>
-              { !readOnly && (
-                  name != "" && (
-                    <span className={badgeClasses}>{badgeText()}</span>
+    <>
+      <div id={`field-${id}`} className={fieldClasses} data-testid="field">
+        <div className="card">
+          <div className="card-body">
+            <div className="d-flex d-row justify-content-between align-items-center">
+              <div>
+                <h5 className="m-0 d-inline">{name}</h5>
+                { !readOnly && (
+                    name != "" && (
+                      <span className={badgeClasses}>{badgeText()}</span>
+                    )
                   )
-                )
-              }
-            </div>
-
-            { !readOnly &&
-            <div className="hstack gap-2">
-              <button
-                className="btn btn-primary"
-                disabled={!isSaveable()}
-                onClick={handleSaveClick}
-              >
-                {saving ? "Saving..." : "Save"}
-              </button>
-              <button
-                className="btn btn-success"
-                disabled={!saved || hasChanged() || running}
-                onClick={handleRunClick}
-              >
-                {running ? "Running..." : "Run field"}
-              </button>
-              <a
-                onClick={handleCollapseExpandClick}
-                className="btn btn-outline-success"
-                data-bs-toggle="collapse"
-                href={`#field-${id}-content`}
-                role="button"
-                aria-expanded={ expanded }
-                aria-controls={`field-${id}-content`}
-              >
-                <ExpandCollapseIcon expanded={expanded} vertical={true} />
-              </a>
-              <button className="btn btn-outline-danger" onClick={handleDeleteClick}>
-                {deleting ? "Deleting..." : "Delete"}
-              </button>
-            </div>
-          }
-          </div>
-
-          <div className="mt-3 collapse show" id={`field-${id}-content`}>
-            <label className="form-label" htmlFor="name">
-              Field Name{" "}
-              <Tooltip data-bs-title="This is the field name that the result of this transformation will appear under on the transformed record.">
-                <i className="bi bi-question-circle" aria-label="help text"></i>
-              </Tooltip>
-            </label>
-            <input
-              id="name"
-              type="text"
-              className="form-control"
-              required="required"
-              placeholder="New field"
-              defaultValue={name}
-              onChange={(e) => setNameValue(e.target.value)}
-            />
-
-            <label className="form-label mt-4" htmlFor="block">
-              Field Block{" "}
-              <Tooltip data-bs-title="This is the code that is applied to create this field on the transformed record.">
-                <i className="bi bi-question-circle" aria-label="help text"></i>
-              </Tooltip>
-            </label>
-            <div id="block" ref={editorRef}></div>
-
-            {error && (
-              <div className="alert alert-danger mt-4" role="alert">
-                <h4 className="alert-heading">{error.title}</h4>
-                <hr />
-                <p className="mb-0">{error.description}</p>
+                }
               </div>
-            )}
+
+              { !readOnly &&
+              <div className="hstack gap-2">
+                <button
+                  className="btn btn-primary"
+                  disabled={!isSaveable()}
+                  onClick={handleSaveClick}
+                >
+                  {saving ? "Saving..." : "Save"}
+                </button>
+                <button
+                  className="btn btn-success"
+                  disabled={!saved || hasChanged() || running}
+                  onClick={handleRunClick}
+                >
+                  {running ? "Running..." : "Run field"}
+                </button>
+                <a
+                  onClick={handleCollapseExpandClick}
+                  className="btn btn-outline-success"
+                  data-bs-toggle="collapse"
+                  href={`#field-${id}-content`}
+                  role="button"
+                  aria-expanded={ expanded }
+                  aria-controls={`field-${id}-content`}
+                >
+                  <ExpandCollapseIcon expanded={expanded} vertical={true} />
+                </a>
+                <button className="btn btn-outline-danger" onClick={handleShow}>
+                  <i class="bi bi-trash" aria-hidden="true"></i>
+                  {deleting ? " Deleting..." : " Delete"}
+                </button>
+              </div>
+            }
+            </div>
+
+            <div className="mt-3 collapse show" id={`field-${id}-content`}>
+              <label className="form-label" htmlFor="name">
+                Field Name{" "}
+                <Tooltip data-bs-title="This is the field name that the result of this transformation will appear under on the transformed record.">
+                  <i className="bi bi-question-circle" aria-label="help text"></i>
+                </Tooltip>
+              </label>
+              <input
+                id="name"
+                type="text"
+                className="form-control"
+                required="required"
+                placeholder="New field"
+                defaultValue={name}
+                onChange={(e) => setNameValue(e.target.value)}
+              />
+
+              <label className="form-label mt-4" htmlFor="block">
+                Field Block{" "}
+                <Tooltip data-bs-title="This is the code that is applied to create this field on the transformed record.">
+                  <i className="bi bi-question-circle" aria-label="help text"></i>
+                </Tooltip>
+              </label>
+              <div id="block" ref={editorRef}></div>
+
+              {error && (
+                <div className="alert alert-danger mt-4" role="alert">
+                  <h4 className="alert-heading">{error.title}</h4>
+                  <hr />
+                  <p className="mb-0">{error.description}</p>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete {name}?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleDeleteClick}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
