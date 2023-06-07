651d0c8e8c433560e19e8ba6bb496d3155002246
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-06-07 22:58:42 +0200

    feat(user_account): added user update and deletion by admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,45 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_admin!
+  before_action :find_user, only: %i[show edit update destroy]
 
   def index
     @users = User.order(username: :desc).page(params[:page])
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def update; end
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'User updated successfully'
+    else
+      flash.alert = 'There was an issue updating the user'
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    if @user.destroy
+      redirect_to users_path, notice: 'User removed successfully'
+    else
+      flash.alert = 'There was an issue deleting the user'
+      redirect_to edit_user_path(@user)
+    end
+  end
 
-  def two_factor_authentication
+  def two_factor_authentication; end
 
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :role)
   end
 end
