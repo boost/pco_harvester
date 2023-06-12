fefd8d4a807ee253076a962357f110833aaa2b5f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2023-06-12 22:24:23 +0200

    feat(user_account): re-styled the 2FA form

---
+++ app/helpers/qr_code_helper.rb
@@ -0,0 +1,13 @@
+module QrCodeHelper
+  def qr_code_as_svg(uri)
+    RQRCode::QRCode.new(
+      uri
+    ).as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 3,
+      standalone: true
+    ).html_safe
+  end
+end
