3e43521108dfa951cb915272de64bd021cab351e
Richard Matthews <richard@boost.co.nz>
2023-08-11 09:03:12 +1200

    Make the pages input required if the user selects set number

--- app/frontend/js/pipeline.js
+++ app/frontend/js/pipeline.js
@@ -1,16 +1,19 @@
 const pipelinePageTypeSelect = document.getElementById('js-pipeline-page-type-select');
 
 if(pipelinePageTypeSelect) {
-  const pipelinePageType = document.getElementById('js-pipeline-page-type');
-  const pipelinePages    = document.getElementById('js-pipeline-pages');
+  const pipelinePageType   = document.getElementById('js-pipeline-page-type');
+  const pipelinePages      = document.getElementById('js-pipeline-pages');
+  const pipelinePagesInput = document.getElementById('harvest_job_pages');
 
   pipelinePageTypeSelect.addEventListener('change', (event) => {
     if(event.target.value == 'all_available_pages') {
       pipelinePageType.setAttribute('class', 'col-7');
       pipelinePages.setAttribute('class', 'col-2 d-none');
+      pipelinePagesInput.removeAttribute('required');
     } else if(event.target.value == 'set_number') {
       pipelinePageType.setAttribute('class', 'col-5');
       pipelinePages.setAttribute('class', 'col-2');
+      pipelinePagesInput.setAttribute('required', 'true');
     }
   });
 }
