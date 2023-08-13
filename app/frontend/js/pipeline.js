3023f75079264be9bc30616abf28c436f8d84bdf
Richard Matthews <richard@boost.co.nz>
2023-08-14 11:08:03 +1200

    Hide the transformation input if a user is not selecting a harvest to run

--- app/frontend/js/pipeline.js
+++ app/frontend/js/pipeline.js
@@ -1,11 +1,15 @@
+import { each } from 'lodash';
+
 const pipelinePageTypeSelect = document.getElementById('js-pipeline-page-type-select');
 
 if(pipelinePageTypeSelect) {
-  const pipelinePageType   = document.getElementById('js-pipeline-page-type');
-  const pipelinePages      = document.getElementById('js-pipeline-pages');
-  const pipelinePagesInput = document.getElementById('harvest_job_pages');
+  const pipelinePageType        = document.getElementById('js-pipeline-page-type');
+  const pipelinePages           = document.getElementById('js-pipeline-pages');
+  const pipelinePagesInput      = document.getElementById('harvest_job_pages');
+  const pipelineHarvestCheckbox = document.getElementsByClassName('js-pipeline-harvest-checkbox')[0]
 
   pipelinePageTypeSelect.addEventListener('change', (event) => {
+
     if(event.target.value == 'all_available_pages') {
       pipelinePageType.setAttribute('class', 'col-7');
       pipelinePages.setAttribute('class', 'col-2 d-none');
@@ -16,4 +20,20 @@ if(pipelinePageTypeSelect) {
       pipelinePagesInput.setAttribute('required', 'true');
     }
   });
+
+  if(pipelineHarvestCheckbox) {
+    const jsTransformationInput = document.getElementsByClassName('js-transformation-input');
+
+    pipelineHarvestCheckbox.addEventListener('click', (event) => {
+      if(event.target.checked) {
+        each(jsTransformationInput, (input) => {
+          input.style.display = 'block';
+        });
+      } else {
+        each(jsTransformationInput, (input) => {
+          input.style.display = 'none'
+        });
+      }
+    });
+  }
 }
