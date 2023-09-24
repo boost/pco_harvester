3efe9e307e722f633f531d02e5701015fafe6138
Richard Matthews <richard@boost.co.nz>
2023-09-25 12:07:55 +1300

    fix(shared_definitions_two): Name Length
    Add length to name uniqueness validation. Otherwise this is throwing an error on staging.

--- db/migrate/20230924223008_add_uniqueness_constraint_to_definition_name.rb
+++ db/migrate/20230924223008_add_uniqueness_constraint_to_definition_name.rb
@@ -1,7 +1,7 @@
 class AddUniquenessConstraintToDefinitionName < ActiveRecord::Migration[7.0]
   def change
-    add_index :extraction_definitions, :name, unique: true
-    add_index :transformation_definitions, :name, unique: true
-    add_index :pipelines, :name, unique: true
+    add_index :extraction_definitions, :name, unique: true, length: 255
+    add_index :transformation_definitions, :name, unique: true, length: 255
+    add_index :pipelines, :name, unique: true, length: 255
   end
 end
