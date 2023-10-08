ca3a50cdb7b4fcb266667cd60301ddba97092d81
Richard Matthews <richard@boost.co.nz>
2023-10-09 10:50:49 +1300

    feat(workflow_enhancements_1): Add Tooltip
    Tell the user why the Add button is disabled and what they can do about it.

--- app/frontend/js/apps/ExtractionApp/components/ParameterNavigationPanel.jsx
+++ app/frontend/js/apps/ExtractionApp/components/ParameterNavigationPanel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { filter, map } from "lodash";
+import { filter } from "lodash";
 
 import { selectAllParameters } from "~/js/features/ExtractionApp/ParametersSlice";
 import { toggleDisplayParameters } from "~/js/features/ExtractionApp/UiParametersSlice";
