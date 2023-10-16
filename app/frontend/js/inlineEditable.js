57406f98d1fc85200519c4214094f97783af9469
Richard Matthews <richard@boost.co.nz>
2023-10-17 09:05:31 +1300

    feat(workflow_enhancements_1): Transformation Definition Record Selector
    Display the settings modal if the record selector is blank and the job is completed.
    Populate the settings modal with the initial data.

--- app/frontend/js/inlineEditable.js
+++ app/frontend/js/inlineEditable.js
@@ -8,8 +8,6 @@ each(inlineEditableControls, (control) => {
   control.addEventListener("click", (event) => {
     const id = event.target.dataset.id;
 
-    console.log(id);
-
     const content = document.getElementById(`${id}-content`);
     const form = document.getElementById(`${id}-form`);
 
