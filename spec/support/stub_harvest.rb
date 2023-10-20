1e3520e90fe8130a8960bcdda82e6120edacbbbb
Richard Matthews <richard@boost.co.nz>
2023-10-20 17:04:19 +1300

    feat(stop_conditions): Initial working of stop conditions for harvest extraction.

--- spec/support/stub_harvest.rb
+++ spec/support/stub_harvest.rb
@@ -11,6 +11,16 @@ def stub_figshare_harvest_requests(request)
       headers: fake_json_headers
     ).to_return(fake_response("figshare_#{page}"))
   end
+
+  # Stub the last page as a duplicate so the harvest finishes
+  stub_request(:get, request.url).with(
+    query: {
+      'page' => 6,
+      'itemsPerPage' => 10,
+      'search_for' => 'zealand'
+    },
+    headers: fake_json_headers
+  ).to_return(fake_response('figshare_5'))
 end
 
 def stub_failed_figshare_harvest_requests(request)
@@ -18,7 +28,7 @@ def stub_failed_figshare_harvest_requests(request)
     stub_request(:get, request.url).with(
       query: {
         'page' => page,
-        'itemsPerPage' => request.extraction_definition.per_page,
+        'itemsPerPage' => request.parameters.find { |parameter| parameter.name == 'itemsPerPage' }.content,
         'search_for' => 'zealand'
       },
       headers: fake_json_headers
@@ -32,6 +42,10 @@ def stub_freesound_harvest_requests(request)
       query: { 'page' => page, 'page_size' => '50' }
     ).to_return(fake_response("freesound_#{page}"))
   end
+
+  stub_request(:get, request.url).with(
+    query: { 'page' => 5, 'page_size' => '50' }
+  ).to_return(fake_response('freesound_4'))
 end
 
 def stub_trove_harvest_requests(request, pages_and_tokens)
@@ -56,4 +70,13 @@ def stub_inaturalist_harvest_requests(extraction_definition, pages_and_tokens)
       headers: fake_json_headers
     ).to_return(fake_response("inaturalist_#{page}"))
   end
+
+  # Stub the last page as a duplicate so the harvest finishes
+  stub_request(:get, extraction_definition.base_url).with(
+    query: {
+      'per_page' => 30,
+      'id_above' => '11267278'
+    },
+    headers: fake_json_headers
+  ).to_return(fake_response('inaturalist_4'))
 end
