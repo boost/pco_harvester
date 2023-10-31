1a9bb844a7534c8e89d59ce0fed962e5bcbae67a
Richard Matthews <richard@boost.co.nz>
2023-10-31 14:41:33 +1300

    feat(large_files): Add split related fields to the extraction definition
    I think this could be refactored in the future if we move the record selector to the harvest block.

--- app/models/extraction_definition.rb
+++ app/models/extraction_definition.rb
@@ -34,6 +34,7 @@ class ExtractionDefinition < ApplicationRecord
   }.freeze
 
   validates :name, uniqueness: true
+  validates :record_selector, presence: true, if: :split?
 
   validates :throttle, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 60_000 }
 
---
+++ db/migrate/20231031013240_add_split_fields_to_extraction_definition.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddSplitFieldsToExtractionDefinition < ActiveRecord::Migration[7.0]
+  def change
+    add_column :extraction_definitions, :split, :boolean, default: false, null: false
+    add_column :extraction_definitions, :record_selector, :string
+  end
+end
