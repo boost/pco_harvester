1ff3909f8d111344cf312875eec8267bfcc988c5
Richard Matthews <richard@boost.co.nz>
2024-01-25 15:23:29 +1300

    Update db schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_16_211621) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_001915) do
   create_table "destinations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "url", null: false
