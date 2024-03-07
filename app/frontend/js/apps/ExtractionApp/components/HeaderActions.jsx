36981a63fc22befc64708bfb48d7e0f386d5b352
Richard Matthews <richard@boost.co.nz>
2024-03-07 14:11:57 +1300

    Prettier

--- app/frontend/js/apps/ExtractionApp/components/HeaderActions.jsx
+++ app/frontend/js/apps/ExtractionApp/components/HeaderActions.jsx
@@ -60,11 +60,12 @@ const HeaderActions = () => {
 
   return createPortal(
     <>
-      {!appDetails.extractionDefinition.split && !appDetails.extractionDefinition.extract_text_from_file && (
-        <button className="btn btn-success me-2" onClick={handlePreviewClick}>
-          <i className="bi bi-play" aria-hidden="true"></i> Preview
-        </button>
-      )}
+      {!appDetails.extractionDefinition.split &&
+        !appDetails.extractionDefinition.extract_text_from_file && (
+          <button className="btn btn-success me-2" onClick={handlePreviewClick}>
+            <i className="bi bi-play" aria-hidden="true"></i> Preview
+          </button>
+        )}
 
       {appDetails.extractionDefinition.split && <RunSample />}
 
