e741c6280b91dd6a32a1837f6a9db773f4777e52
Richard Matthews <rj.matthews23@gmail.com>
2024-05-07 13:17:18 +1200

    feat(enrichments update): allow specifying the source id and the key of a fragment when the enrichment URL is full in the fragment

---
+++ db/migrate/20240506215031_add_source_id_and_fragment_key_to_extraction_definition.rb
@@ -0,0 +1,6 @@
+class AddSourceIdAndFragmentKeyToExtractionDefinition < ActiveRecord::Migration[7.0]
+  def change
+    add_column :extraction_definitions, :fragment_source_id, :string
+    add_column :extraction_definitions, :fragment_key, :string
+  end
+end
